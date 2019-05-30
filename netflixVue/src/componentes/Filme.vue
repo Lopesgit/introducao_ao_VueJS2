a16129dc5a332811df4c6c84ee1f338b1c171f4d
Renê Lopes <renelopes.over@gmail.com>
2019-05-30 13:45:23 -0300

    Commit inicial

---
+++ netflixVue/src/componentes/Filme.vue
@@ -0,0 +1,39 @@
+<template>
+
+	<div class="gui-card" v-bind:style="efeitoClick" v-on:click="clickEfeito()" v-on:mouseout="removeClickEfeito()">
+	    <div class="gui-card__media">
+	     	<img class="gui-card__img" v-bind:src="imagem" alt=""  />
+	    </div>
+	    <div class="gui-card__details">
+	     	<div class="gui-card__title">
+	        	{{titulo}}
+	      	</div>
+	    </div>
+	</div>
+
+</template>
+
+<script>
+export default{
+	props:['titulo','imagem'],
+	data(){
+		return{
+			efeitoClick:null
+		}
+	},
+	methods:{
+		clickEfeito: function(){
+			console.log("clickEfeito");
+			this.efeitoClick = 'transform: scale(1.9); margin-right: 110px;';
+		},
+		removeClickEfeito: function(){
+			console.log("removeClickEfeito");
+			this.efeitoClick = null;
+		}
+	}
+}
+</script>
+
+<style lang="scss">
+
+</style>
